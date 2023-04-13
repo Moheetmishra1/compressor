11f3a3867c9e3eb15316f45e68adb62eb7229b19
hp <moheetmishra.gaya@gmail.com>
2023-04-13 21:51:23 +0530

    complete the exit button

--- gui.py
+++ gui.py
@@ -2,40 +2,52 @@ import functionforpyproject
 import PySimpleGUI as sg
 import time
 now = time.strftime("%b-%d,%Y %H:%M:%S")
-print("It is ",now)
+print("It is ", now)
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter tool",key="todo")
+input_box = sg.InputText(tooltip="Enter tool", key="todo")
 add_button = sg.Button("Add")
-list_box = sg.Listbox(values=functionforpyproject.get_todos(),key='todos',
+list_box = sg.Listbox(values=functionforpyproject.get_todos(), key='todos',
                       enable_events=True,size=[45,10])
 edit_button = sg.Button("Edit")
+Complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
 window = sg.Window("My To-do app",
-                   layout=[[label],[input_box,add_button],[list_box,edit_button]],
-                   font=('Helvetical',20))
+                   layout=[[label], [input_box, add_button], [list_box, edit_button, Complete_button],
+                   [exit_button]],
+                   font=('Helvetical', 20))
 while True:
-    event,values=window.read()
+    event, values=window.read()
     print(event)  #This method display the window
     print(values)
     match event:
         case "Add":
-            todos = functionforpyproject.get_todos()
+            List = functionforpyproject.get_todos()
             new_todo = values['todo']+f"[{now}]"+'\n'
-            todos.append(new_todo)
-            functionforpyproject.write_todos(todos)
-            window['todos'].update(values = todos)
+            List.append(new_todo)
+            functionforpyproject.write_todos(List)
+            window['todos'].update(values=List)
         case "Edit":
              todo_to_edit = values["todos"][0]
 
              new_todo = values["todo"]+f"[{now}]"'\n'
 
-             todos = functionforpyproject.get_todos()
-             index = todos.index(todo_to_edit)
-             todos[index] = new_todo
-             functionforpyproject.write_todos(todos)
-             window['todos'].update(values=todos)
+             List = functionforpyproject.get_todos()
+             index = List.index(todo_to_edit)
+             List[index] = new_todo
+             functionforpyproject.write_todos(List)
+             window['todos'].update(values=List)
         case 'todos':
-            window['todo'].update(value=values['todos'][0])
+           window['todo'].update(value=values['todos'][0])
+        case 'Complete':
+            lists = functionforpyproject.get_todos()
+            lists.remove(values['todos'][0])
+            functionforpyproject.write_todos(lists)
+            window['todos'].update(values=lists)
+            window["todo"].update(value=' ')
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
 
